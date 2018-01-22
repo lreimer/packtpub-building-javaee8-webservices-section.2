12d1b56027ea8423994db597434e2fd0374f133e
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-01-22 13:30:10 +0100

    Added comments.

--- src/main/java/com/packtpub/javaee8/integration/EntityManagerProducer.java
+++ src/main/java/com/packtpub/javaee8/integration/EntityManagerProducer.java
@@ -1,6 +1,5 @@
 package com.packtpub.javaee8.integration;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
@@ -8,18 +7,32 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
-@ApplicationScoped
+/**
+ * CDI producer bean to manage {@link EntityManager} instances for
+ * our H2 persistence unit.
+ */
 public class EntityManagerProducer {
 
     @PersistenceUnit
     private EntityManagerFactory entityManagerFactory;
 
+    /**
+     * Create an {@link EntityManager} instance for our persistence unit
+     * and make it injectable as a CDI bean.
+     *
+     * @return the instance
+     */
     @Produces
     @RequestScoped
     public EntityManager createEntityManager() {
         return entityManagerFactory.createEntityManager();
     }
 
+    /**
+     * Dispose and close the given {@link EntityManager} instance.
+     *
+     * @param entityManager the instance to close
+     */
     public void disposeEntityManager(@Disposes EntityManager entityManager) {
         entityManager.close();
     }
