7b4318a220151ba56d866502b030aaef69f1f09f
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-01-23 14:30:39 +0100

    Fixed wrong author.

--- src/main/resources/META-INF/books.sql
+++ src/main/resources/META-INF/books.sql
@@ -1,6 +1,6 @@
 INSERT INTO book (isbn, title, author) VALUES ('0345391802', 'The Hitchhiker''s Guide to the Galaxy', 'Douglas Adams')
 INSERT INTO book(isbn, title, author) VALUES ('1788393856', 'Architecting Modern Java EE Applications', 'Sebastian Daschner')
-INSERT INTO book(isbn, title, author) VALUES ('1788293673', 'Java EE 8 Application Development', 'Douglas Adams')
+INSERT INTO book(isbn, title, author) VALUES ('1788293673', 'Java EE 8 Application Development', 'David R. Heffelfinger')
 INSERT INTO book(isbn, title, author) VALUES ('1786469200', 'Mastering Java EE 8 Application Development', 'Kapila Bogahapitiya,  Sandeep Nair')
 
 INSERT INTO loan(id, username, book_id) VALUES ('31d8dbdc-82c0-4c19-ab9b-b830b87472c4', 'mario-leander.reimer', '0345391802')
